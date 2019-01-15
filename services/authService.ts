1791feddd11c914bb2c6e22f27a3eb029d2edcee
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-15 16:46:27 +0000

    modified API service

--- services/authService.ts
+++ services/authService.ts
@@ -1,23 +1,9 @@
 import { CognitoIdentity, CognitoIdentityServiceProvider } from "aws-sdk";
 
 export class authService {
-    cognitoIdentity: CognitoIdentity = new CognitoIdentity;
 
-    cognitoUsername: string;
-    
-    constructor(cognitoUsername: string){
-        this.cognitoUsername = cognitoUsername;
-    }
+    static isAPIKeyValid = () => {
 
-    isUserValid(): boolean {
-        // Check if user is valid
-        return true;
-    }
-
-    getUserId(): string {
-        
-        // Get user Id from Cognito
-        return "";
     }
 
 
