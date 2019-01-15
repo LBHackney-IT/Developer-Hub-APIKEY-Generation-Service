7854460e81c49e02d08d1aec927950a5bba0f465
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-15 12:55:46 +0000

    sett up mail service

--- services/mailService.ts
+++ services/mailService.ts
@@ -1,3 +1,25 @@
-export class mailService {
+import { SES } from "aws-sdk";
 
+export class mailService {
+    private ses: SES;
+
+    constructor() {
+        this.ses = new SES({apiVersion: '2010-12-01'});
+    }
+
+    async sendEmail() : Promise<any> {
+        const params = {
+            Destination: {
+                CcAddresses: [
+
+                ],
+                ToAddresses: [
+
+                ]
+            },
+            Source: "",
+            
+        };
+
+    }
 }
