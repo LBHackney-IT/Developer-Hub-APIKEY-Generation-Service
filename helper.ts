38af0a5612aee93f7e23100d644565e604b86f08
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-08 00:21:23 +0000

    added path to build constructor

--- helper.ts
+++ helper.ts
@@ -12,16 +12,17 @@ export const assignToBody = (item: Object | Array<any> | String): object => {
 
 export const createPathKey = (pathObject: object): IPath[] => {
     const keys = Object.keys(pathObject);
-    console.log('keys',keys)
     const paths: IPath[] = keys.map((key) => {
         const url: string = key;
         const requestType: string = Object.keys(pathObject[url])[0];
+        const id: string = pathObject[url][requestType]['operationId'];
         const tags: string[] = pathObject[url][requestType]['tags'];
         const summary: string = pathObject[url][requestType]['summary'];
         const parameters: IPathParameter[] = pathObject[url][requestType]['parameters'];
         const responses: object = pathObject[url][requestType]['responses'];
 
         return {
+            id: id,
             url: url,
             requestType: requestType,
             tags: tags,
