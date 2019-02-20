405ef99139746ca6598192a00e0a086bf42080a5
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-20 17:02:53 +0000

    updated tests

--- tests/helper.spec.ts
+++ tests/helper.spec.ts
@@ -1,6 +1,6 @@
 import 'mocha';
 import * as chai from 'chai';
-import { generateID } from '../helper';
+import { generateID, assignToBody, createPathKey } from '../helper';
 
 const expect = chai.expect;
 describe('Helper File', () => {
@@ -12,4 +12,89 @@ describe('Helper File', () => {
             expect(result).to.equal('xxx_apiID');
         });
     });
+
+    describe('assignToBody', () => {
+        it('should return an object with the key (body)', () => {
+            const item = 'item';
+            const result = assignToBody(item);
+            expect(result).to.have.key('body');
+        });
+    });
+
+    describe('createPathKey', () => {
+        it('should return an array of objects that have IPath', () => {
+            const pathObject = {
+                "/v1/work_orders/{workOrderReference}/available_appointments": {
+                    "get": {
+                        "tags": ["Appointments"],
+                        "summary": "Returns available appointments for a Universal Housing work order",
+                        "operationId": "V1Work_ordersByWorkOrderReferenceAvailable_appointmentsGet",
+                        "consumes": [],
+                        "produces": [],
+                        "parameters": [
+                            { "name": "workOrderReference", "in": "path", "required": true, "type": "string" }
+                        ],
+                        "responses": {
+                            "200": { "description": "Returns the list of available appointments" },
+                            "400": { "description": "If no valid work order reference is provided" },
+                            "404": { "description": "Not Found" },
+                            "500": { "description": "If any errors are encountered" }
+                        }
+                    }
+                },
+                "/v1/work_orders/{workOrderReference}/appointments": {
+                    "get": {
+                        "tags": ["Appointments"],
+                        "summary": "Returns all appointments for a work order",
+                        "operationId": "V1Work_ordersByWorkOrderReferenceAppointmentsGet",
+                        "consumes": [],
+                        "produces": [],
+                        "parameters": [{ "name": "workOrderReference", "in": "path", "description": "UH work order reference", "required": true, "type": "string" }], "responses": { "200": { "description": "Returns a list of appointments for a work order reference" }, "404": { "description": "If there are no appointments found for the work orders reference" }, "500": { "description": "If any errors are encountered" } }
+                    }, "post": {
+                        "tags": ["Appointments"],
+                        "summary": "Creates an appointment",
+                        "operationId": "V1Work_ordersByWorkOrderReferenceAppointmentsPost",
+                        "consumes": [
+                            "application/json-patch+json",
+                            "application/json",
+                            "text/json",
+                            "application/*+json"
+                        ],
+                        "produces": [],
+                        "parameters": [
+                            { "name": "workOrderReference", "in": "path", "description": "The reference number of the work order for the appointment", "required": true, "type": "string" },
+                            { "name": "request", "in": "body", "required": false, "schema": { "$ref": "#/definitions/ScheduleAppointmentRequest" } }
+                        ],
+                        "responses": {
+                            "200": { "description": "A successfully created repair request" }
+                        }
+                    }
+                },
+                "/v1/work_orders/{workOrderReference}/appointments/latest": {
+                    "get": {
+                        "tags": ["Appointments"],
+                        "summary": "Returns the latest apointment for a work order",
+                        "operationId": "V1Work_ordersByWorkOrderReferenceAppointmentsLatestGet",
+                        "consumes": [],
+                        "produces": [],
+                        "parameters": [
+                            { "name": "workOrderReference", "in": "path", "description": "UH work order reference", "required": true, "type": "string" }
+                        ],
+                        "responses": {
+                            "200": { "description": "Returns an appointment for a work order reference" },
+                            "404": { "description": "If there is no appointment found for the work order reference" },
+                            "500": { "description": "If any errors are encountered" }
+                        }
+                    }
+                }
+            };
+
+            const result = createPathKey(pathObject);
+
+            expect(result).to.be.an('array');
+            expect(result[0]).to.be.an('object');
+        });
+    });
+
+
 });
