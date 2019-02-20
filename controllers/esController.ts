6819657f89c2e6ef1300deb4c4af8a2ad5a88d7a
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-20 16:43:10 +0000

    added comments

--- controllers/esController.ts
+++ controllers/esController.ts
@@ -7,40 +7,49 @@ import { elasticSearchService } from '../services/elasticSearchService';
 
 export const dynamoDBStreamToEs: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent, context) => {
     try {
-        const convert = DynamoDB.Converter.output;
+        // Instantiate AWS.DynamoDB.Unmarshall converter
+        const convert = DynamoDB.Converter.unmarshall;
+        // Instantiate Elastic Search Service
         const esService: elasticSearchService = new elasticSearchService();
+        // Get Elastic search index
         let index: string = process.env.ELASTIC_INDEX_API;
-        event.Records.forEach((record) => {
-            
+
+        // Loop through each record
+        event.Records.forEach(async (record) => {
+            // Check the eventName for each record and perform action
             switch (record.eventName) {
+                // Index new object when created in DynamoDB
                 case 'INSERT': {
+                    // Convert from AWS Object to JS Object
                     const newObject = convert(record.dynamodb.NewImage);
-                    esService.index(newObject, index)
-                    .then((data) => {
-                        console.log(data);
-                    }).catch((error) => {
-                        throw new Error(error.message)
-                    });
+                    await esService.index(newObject, index)
+                        .then((data) => {
+                            console.log('success', data);
+                        }).catch((error) => {
+                            throw new Error(error.message)
+                        });
                     break;
                 }
+                // Index object when modified in DynamoDB                
                 case 'MODIFY': {
                     const newObject = convert(record.dynamodb.NewImage);
-                    esService.index(newObject, index)
-                    .then((data) => {
-                        console.log(data);
-                    }).catch((error) => {
-                        throw new Error(error.message)
-                    });
+                    await esService.index(newObject, index)
+                        .then((data) => {
+                            console.log(data);
+                        }).catch((error) => {
+                            throw new Error(error.message)
+                        });
                     break;
                 }
+                // delete object from index when removed in DynamoDB                
                 case 'REMOVE': {
                     const newObject = convert(record.dynamodb.NewImage);
-                    esService.delete(newObject, index)
-                    .then((data) => {
-                        console.log(data);
-                    }).catch((error) => {
-                        throw new Error(error.message)
-                    });
+                    await esService.delete(newObject, index)
+                        .then((data) => {
+                            console.log(data);
+                        }).catch((error) => {
+                            throw new Error(error.message)
+                        });
                     break;
                 }
             }
@@ -50,7 +59,7 @@ export const dynamoDBStreamToEs: DynamoDBStreamHandler = async (event: DynamoDBS
 
 
     } catch (error) {
-        console.log({message: error.message, code: error.statusCode});
+        console.log({ message: error.message, code: error.statusCode });
     }
 
 }
@@ -63,28 +72,28 @@ export const indexES: APIGatewayProxyHandler = async (event, context) => {
         const esService: elasticSearchService = new elasticSearchService();
         const db: dbService = new dbService(DATABASE_ID);
         await db.getAllItems()
-        .then((data) => {
-            listOfApis = data.Items
-        })
-        .catch((error) => {
-            throw new Error(error.message);
-        });
-
-        listOfApis.forEach( async (api) => {
-            await esService.index(api, process.env.ELASTIC_INDEX_API)
-            .then(() => {
-                response = {
-                    [api.id] : 'successful'
-                }
+            .then((data) => {
+                listOfApis = data.Items
             })
             .catch((error) => {
-                throw new Error('Elastic search cluster is down');
+                throw new Error(error.message);
             });
+
+        listOfApis.forEach(async (api) => {
+            await esService.index(api, process.env.ELASTIC_INDEX_API)
+                .then(() => {
+                    response = {
+                        [api.id]: 'successful'
+                    }
+                })
+                .catch((error) => {
+                    throw new Error('Elastic search cluster is down');
+                });
         });
 
         return responseService.success(response);
-    
-    } catch(error) {
+
+    } catch (error) {
         return responseService.error(error.message, error.statusCode);
     };
 }
