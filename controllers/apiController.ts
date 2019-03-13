5c8033cb1d69b799e5584b73adcc0c2427dda28b
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-03-13 11:17:31 +0000

    added await to async function

--- controllers/apiController.ts
+++ controllers/apiController.ts
@@ -14,11 +14,15 @@ export const createApi: APIGatewayProxyHandler = async (event, context) => {
     try {
         const body = JSON.parse(event.body);
         const api: IApi = body;
+        if(!api.id) {
+            api.id = api.title.replace(' ', '_').toLowerCase();
+        }
+        console.log(api);
         if (Object.keys(api).length == 0) {
             throw new Error("Request object is missing");
         }
         const apiClass: Api = new Api();
-        const response = apiClass.create(api);
+        const response = await apiClass.create(api);
         return responseService.success(response);
     } catch (error) {
         return responseService.error(error.message, error.statusCode);
@@ -58,7 +62,7 @@ export const deleteApi: APIGatewayProxyHandler = async (event, context) => {
             throw new Error("Request variable is missing");
         }
         const api: Api = new Api();
-        const response = api.delete(id);
+        const response = await api.delete(id);
 
         return responseService.success(response);
 
