808d1b41062d8a14a02cfec381af0ad67dfa13b0
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-03-20 10:21:27 +0000

    updated

--- classes/Swagger.ts
+++ classes/Swagger.ts
@@ -18,22 +18,22 @@ export class Swagger {
             let response;
             const esService: elasticSearchService = new elasticSearchService();
             await esService.getItem(apiId, this.SWAGGER_INDEX)
-            .then((data) => {
-                const swaggerObject: ISwagger = data._source
-                const path = swaggerObject.paths.filter((path) => {
-                    return path.id === pathId;
+                .then((data) => {
+                    const swaggerObject: ISwagger = data._source
+                    const path = swaggerObject.paths.filter((path) => {
+                        return path.id === pathId;
+                    });
+                    swaggerObject.paths = path;
+                    response = assignToBody(swaggerObject);
+                })
+                .catch((error) => {
+                    throw new Error(error.message)
                 });
-                swaggerObject.paths = path;
-                response = assignToBody(swaggerObject);
-            })
-            .catch((error) => {
-                throw new Error(error.message)
-            });
 
             return response;
-             
+
         } catch (error) {
-            
+
         }
     }
 
@@ -46,19 +46,19 @@ export class Swagger {
         try {
             let response;
             const esService: elasticSearchService = new elasticSearchService();
-            
-            await esService.getSwaggerObjects(this.SWAGGER_INDEX)
-            .then((data) => {
-                response = data.hits.hits;
-                response = response.map((item) => {
-                    return item['_source'];
-                });
 
-                response = assignToBody(response);
-            })
-            .catch((error) => {
-                throw new Error(error.message);
-            });
+            await esService.getSwaggerObjects(this.SWAGGER_INDEX)
+                .then((data) => {
+                    response = data.hits.hits;
+                    response = response.map((item) => {
+                        return item['_source'];
+                    });
+
+                    response = assignToBody(response);
+                })
+                .catch((error) => {
+                    throw new Error(error.message);
+                });
 
             return response;
         } catch (error) {
@@ -71,76 +71,69 @@ export class Swagger {
             let response;
             const esService: elasticSearchService = new elasticSearchService();
             await esService.getSwaggerUrls(this.API_INDEX)
-            .then((data) => {
-                response = data.hits.hits;
-                response = response.map((item) => {
-                    return item['_source'];
-                });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-
-            // Make Request to Each Swagger URL
-        response = await Promise.all(response.map(async (urlObject) => {  
-            // Check if swagger url string contains .json
-            const regexResult = RegExp(this.endInJsonRegex).test(urlObject.production.swagger_url)
-            let swaggerObject: ISwagger;
-            // If swagger url contains .json get the swagger object
-            if (regexResult) {
-                // Make a get request to each url then make an object(ISwagger)
-                await Axios.get(urlObject.production.swagger_url)
-                .then((_response) => {
-                    urlObject.swagger = _response.data;
-                    swaggerObject = {
-                        id: urlObject.id,
-                        title: _response.data.info.title,
-                        version: _response.data.info.version,
-                        description: _response.data.info.description || null,
-                        paths: createPathKey(_response.data.paths),
-                        last_updated: Date.now()
-                    };
-                }).catch((error) => {
+                .then((data) => {
+                    response = data.hits.hits;
+                    response = response.map((item) => {
+                        return item['_source'];
+                    });
+                })
+                .catch((error) => {
                     console.log(error);
                 });
 
-                return swaggerObject;
-             } else {
-            // If swagger url contains .json get the swagger object
-                return null;
-            }
-        }));
+            // Make Request to Each Swagger URL
+            response = await Promise.all(response.map(async (urlObject) => {
+                // Check if swagger url string contains .json
+                const regexResult = RegExp(this.endInJsonRegex).test(urlObject.production.swagger_url)
+                let swaggerObject: ISwagger;
+                // If swagger url contains .json get the swagger object
+                if (regexResult) {
+                    // Make a get request to each url then make an object(ISwagger)
+                    await Axios.get(urlObject.production.swagger_url)
+                        .then((_response) => {
+                            urlObject.swagger = _response.data;
+                            swaggerObject = {
+                                id: urlObject.id,
+                                title: _response.data.info.title,
+                                version: _response.data.info.version,
+                                description: _response.data.info.description || null,
+                                paths: createPathKey(_response.data.paths),
+                                last_updated: Date.now()
+                            };
+                        }).catch((error) => {
+                            console.log(error);
+                        });
 
-                // response = swaggerFiles.map((swaggerFile) => {
-        //     return {
-        //         id: swaggerFile.id,
-        //         title: swaggerFile.info.title,
-        //         version: swaggerFile.info.version,
-        //         description: swaggerFile.info.description,
-        //         paths: createPathKey(swaggerFile.paths),
-        //         last_updated: Date.now()
-        //     };
-        // });
+                    return swaggerObject;
+                } else {
+                    // If swagger url contains .json get the swagger object
+                    return null;
+                }
+            }).filter((item) => {
+                return item !== null;
+            }));
 
-        response = response.filter((item) => {
-            return item !== null;
-        });
 
-        // Add Objects To Elastic Search
+            // response = response.filter((item) => {
+            //     return item !== null;
+            // });
 
-        response.forEach(async (item) => {
-            await esService.index(item, this.SWAGGER_INDEX)
-                .then((data) => {
-                    console.log(data)
-                })
-                .catch((error) => {
-                    throw new Error(error.message);
-                })
-        });
+            console.log(1, response);
 
-        // return responseService.success(response);
+            // Add Objects To Elastic Search
+
+            response.forEach(async (item) => {
+                await esService.index(item, this.SWAGGER_INDEX)
+                    .then((data) => {
+                        console.log(2, data)
+                    })
+                    .catch((error) => {
+                        throw new Error(error.message);
+                    })
+            });
+
         } catch (error) {
-            
+            throw new Error(error.message);
         }
     }
 }
